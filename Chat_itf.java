0ce9a9ad1db37e5b8de750141a8d22608102ab9b
Vaynee-Sungeelee <45771355+Vaynee-Sungeelee@users.noreply.github.com>
2019-02-16 18:24:38 +0100

    Question 2

--- Chat_itf.java
+++ Chat_itf.java
@@ -1,10 +1,13 @@
 import java.rmi.*;
 
 public interface Chat_itf extends Remote{
-    public boolean register(Client_itf client) throws RemoteException;
-    public void unRegister(Client_itf client) throws RemoteException;
-    public void sendMsg(Client_itf target, String sender, String msg) throws RemoteException;
-    public void broadcastMsg(String clientname , String message) throws RemoteException;
-    public Client_itf getClient(String name) throws RemoteException;
-
+    boolean register(Client_itf client) throws RemoteException;
+    void unRegister(Client_itf client) throws RemoteException;
+    void sendMsg(Client_itf target, String sender, String msg) throws RemoteException;
+    void broadcastMsg(String clientName , String message) throws RemoteException;
+    Client_itf getClient(String name) throws RemoteException;
+    void addChatRoom(ChatRoom_itf chatRoom) throws RemoteException;
+    void getAllChatRooms(Client_itf client) throws RemoteException;
+    ChatRoom_itf getChatRoom(String chatRoomTitle) throws RemoteException;
+    void getHistory(Client_itf client) throws RemoteException;
 }

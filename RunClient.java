0b99cb7d55539323adcf9a38c66f6103e442d68d
Vaynee-Sungeelee <45771355+Vaynee-Sungeelee@users.noreply.github.com>
2019-02-16 11:49:21 +0100

    Added registration check

--- RunClient.java
+++ RunClient.java
@@ -30,6 +30,9 @@ public class RunClient {
 
             boolean notExit = true;
 
+
+            //System.out.println("notExit" + notExit);
+
             Client c = null;
 
             if(clientName != null) {
@@ -37,32 +40,45 @@ public class RunClient {
 
             }else{
                 System.out.println("You must enter a name");
-                System.exit(-1);
             }
 
+            //System.out.println("notExit2" + notExit);
 
             String msg = null;
-            while (notExit) {
+
+            if(c.isRegistered()) {
                 System.out.println("Type 0 to exit");
                 System.out.println("Type 1 followed by your message to send to all clients");
                 System.out.println("Type 2 followed by a client's name and your message to send to that client");
-                msg = scanner.nextLine().trim();
-                System.out.println("startswith 0: "+msg.startsWith("0"));
-                if(msg.startsWith("0")){
-                    //server.unRegister(c);
-                    notExit = false;
-                }else if(msg.startsWith("1")){
-                    server.broadcastMsg(clientName,msg);
-                }else if(msg.startsWith("2")){
-                    String username = msg.split(" ")[1];
-                    Client_itf target = server.getClient(username);
-                    server.sendMsg(target,clientName,msg);
+
+                while (notExit) {
+
+                    msg = scanner.nextLine().trim();
+                    //System.out.println("startswith 0: " + msg.startsWith("0"));
+                    if (msg.startsWith("0")) {
+                        //server.unRegister(c);
+                        notExit = false;
+                    } else if (msg.startsWith("1")) {
+                        server.broadcastMsg(clientName, msg);
+                    } else if (msg.startsWith("2")) {
+                        String username = msg.split(" ")[1];
+                        Client_itf target = server.getClient(username);
+                        if(target != null) {
+                            server.sendMsg(target, clientName, msg);
+                        }else{
+                            System.out.println("The client "+ username +"doesn't exist");
+                        }
+                    }
                 }
+                System.out.println("Successfully disconnected client: " + c.getName());
+
+            }else{
+                System.out.println("Error in registration process");
             }
-            System.out.println("Successfully disconnected client: " + c.getName());
+
 
         } catch (Exception e) {
-            System.out.println("[System] Server failed: " + e);
+            System.out.println("Server failed: " + e);
         }
     }
 

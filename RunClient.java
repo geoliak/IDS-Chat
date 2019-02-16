92ac81365d830edc2c19eb4f1bad5585d5ed50f3
Vaynee-Sungeelee <45771355+Vaynee-Sungeelee@users.noreply.github.com>
2019-02-16 18:27:12 +0100

    Question 2

--- RunClient.java
+++ RunClient.java
@@ -47,29 +47,71 @@ public class RunClient {
             String msg = null;
 
             if(c.isRegistered()) {
-                System.out.println("Type 0 to exit");
-                System.out.println("Type 1 followed by your message to send to all clients");
-                System.out.println("Type 2 followed by a client's name and your message to send to that client");
+                System.out.println("Type 0 and press Enter to exit");
+                System.out.println("Type 1 followed by your message to send to all clients and press Enter");
+                System.out.println("Type 2 to get a history of all messages sent by server");
+                System.out.println("Type 3 followed by a client's name and your message to send to that client and press Enter");
+                System.out.println("Type 4 to view available chat rooms and press Enter");
+                System.out.println("Type 5 followed by the chat room name to join the chat room and press Enter");
+                System.out.println("Type 6 followed by the chat room name to consult the chat room's history and press Enter");
+                System.out.println("Type 7 followed by the chat room name to send a message to the chat room and press Enter");
+                System.out.println("Type 8 followed by the chat room name to leave the chat room and press Enter");
 
                 while (notExit) {
 
                     msg = scanner.nextLine().trim();
                     //System.out.println("startswith 0: " + msg.startsWith("0"));
                     if (msg.startsWith("0")) {
-                        //server.unRegister(c);
                         notExit = false;
                     } else if (msg.startsWith("1")) {
                         server.broadcastMsg(clientName, msg);
                     } else if (msg.startsWith("2")) {
+                        server.getHistory(c);
+                    } else if (msg.startsWith("3")) {
                         String username = msg.split(" ")[1];
                         Client_itf target = server.getClient(username);
                         if(target != null) {
-                            server.sendMsg(target, clientName, msg);
+                            server.sendMsg(target, clientName, msg.substring(username.length()+2));
                         }else{
-                            System.out.println("The client "+ username +"doesn't exist");
+                            System.out.println("The client "+ username +" doesn't exist");
+                        }
+                    }else if(msg.startsWith("4")){
+                        server.getAllChatRooms(c);
+                    }else if(msg.startsWith("5")){//join chatroom
+                        String chatRoomTitle = msg.substring(2);
+                        ChatRoom_itf chatRoom = server.getChatRoom(chatRoomTitle);
+                        if(chatRoom != null) {
+                            chatRoom.addUser(c);
+                        }else{
+                            System.out.println("The chat room "+ chatRoomTitle +" doesn't exist");
+                        }
+                    }else if(msg.startsWith("6")) {
+                        String chatRoomTitle = msg.substring(2);
+                        ChatRoom_itf chatRoom = server.getChatRoom(chatRoomTitle);
+                        if(chatRoom != null && chatRoom.getUser(c) != null) {
+                            chatRoom.printAllMessages(c);
+                        }else{
+                            System.out.println("You are not a member of "+ chatRoomTitle +" or it doesn't exist");
+                        }
+                    }else if(msg.startsWith("7")) {
+                        String chatRoomTitle = msg.substring(2);
+                        ChatRoom_itf chatRoom = server.getChatRoom(chatRoomTitle);
+                        if(chatRoom != null && chatRoom.getUser(c) != null) {
+                            chatRoom.addMessage(clientName,msg.substring(chatRoomTitle.length()+2));
+                        }else{
+                            System.out.println("You are not a member of "+ chatRoomTitle +" or it doesn't exist");
+                        }
+                    }else if(msg.startsWith("8")) {
+                        String chatRoomTitle = msg.substring(2);
+                        ChatRoom_itf chatRoom = server.getChatRoom(chatRoomTitle);
+                        if(chatRoom != null && chatRoom.getUser(c) != null) {
+                            chatRoom.removeUser(c);
+                        }else{
+                            System.out.println("You are not a member of "+ chatRoomTitle);
                         }
                     }
                 }
+                server.unRegister(c);
                 System.out.println("Successfully disconnected client: " + c.getName());
 
             }else{

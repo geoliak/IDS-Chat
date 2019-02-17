75cbc96366654a65050491e4286e282013f3628d
Vaynee-Sungeelee <45771355+Vaynee-Sungeelee@users.noreply.github.com>
2019-02-17 02:05:36 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -6,32 +6,42 @@ import java.util.*;
 
 public class Client extends UnicastRemoteObject implements Client_itf{
 
-    private String name = null;
+    private String name;
     private Chat_itf chatInterface;
-    private boolean registered = false;
+    private boolean registered;
+    private ChatRoom_itf chatRoom;
 
     public Client(String name, Chat_itf cItf) throws RemoteException{
         //System.out.println("in constructor ");
         this.name = name;
         this.chatInterface = cItf;
         registered = cItf.register(this);
+        this.chatRoom = null;
     }
 
     public boolean isRegistered(){
+
         return registered;
     }
 
-    public void sendMsg(String sender, String msg) throws RemoteException{
+    public void sendMsg(String sender, String msg){
         if(sender != null){
-            System.out.println("["+sender+"]"+ msg);//other client's msg
+            System.out.println("["+sender+"]: "+ msg);//other client's msg
         }else{
             System.out.println(msg);//server msg
         }
     }
 
-    public String getName() throws RemoteException{
+    public String getName(){
         //System.out.println("in getName ");
         return this.name;
     }
 
+    public void setChatRoom(ChatRoom_itf chatRoom) {
+        this.chatRoom = chatRoom;
+    }
+
+    public ChatRoom_itf getChatRoom() {
+        return this.chatRoom;
+    }
 }

10ee2d942ae216569bcf008e270c5f93fe5d0fe1
Vaynee-Sungeelee <45771355+Vaynee-Sungeelee@users.noreply.github.com>
2019-02-17 02:04:01 +0100

    Update Client_itf.java

--- Client_itf.java
+++ Client_itf.java
@@ -1,6 +1,8 @@
 import java.rmi.*;
 
 public interface Client_itf extends Remote{
-    public void sendMsg(String sender, String msg) throws RemoteException;
-    public String getName() throws RemoteException;
+    void sendMsg(String sender, String msg) throws RemoteException;
+    String getName() throws RemoteException;
+    void setChatRoom(ChatRoom_itf chatRoom) throws RemoteException;
+    ChatRoom_itf getChatRoom() throws RemoteException;
 }

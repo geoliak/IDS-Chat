8b93412ac43f8c6d05e758530740ccc54a6ce6d0
geoliak <geoliak@users.noreply.github.com>
2019-03-12 20:21:49 +0100

    Repo cleanup

--- Client_itf.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface Client_itf extends Remote{
-    public void sendMsg(String sender, String msg) throws RemoteException;
-    public String getName() throws RemoteException;
-}
